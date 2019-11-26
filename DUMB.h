b10f0a29f0c61888b6256ef963a478cd52fa5ba8
Matthew Hwang <mh1160@grep.cs.rutgers.edu>
2019-11-26 17:36:54 -0500

    second commit

---
+++ DUMB.h
@@ -0,0 +1,20 @@
+#ifndef _DUMB_h
+#define _DUMB_h
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<errno.h>
+#include<dirent.h>
+#include<fcntl.h>
+#include<openssl/sha.h>
+#include<netinet/in.h>
+#include<math.h>
+
+#endif
