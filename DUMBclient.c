ecb1db8c12df4030401c0a8c2847046dd7de0740
Noah M. Lipton <nml109@ice.cs.rutgers.edu>
2019-12-06 16:15:16 -0500

    First round of client

--- DUMBclient.c
+++ DUMBclient.c
@@ -4,12 +4,20 @@
 #include<arpa/inet.h>
 #include<sys/types.h>
 #include<netdb.h>
+#include<string.h>
 
 int main(int argc, char** argv){
   struct addrinfo hints, *addrinfo;
+  char buffer[1024] = {0};
+  int valread;
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   addrinfo = getaddrinfo(argv[1], argv[2], &hints, &addrinfo);
   int sockfd = socket(addrinfo->ai_family, addrinfo->ai_socktype, 0);
   connect(sockfd, addrinfo->ai.addr, addrinfo->ai.addrlen);
+  send(sockfd, HELLO, strlen(HELLO), 0);
+  printf("HELLO message sent\n");
+  valread = read(sockfd, buffer, 1024);
+  printf("%s\n", buffer);
+  return 0;
 }
